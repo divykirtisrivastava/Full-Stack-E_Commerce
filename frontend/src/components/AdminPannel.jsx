d868d08a9383cc3f57ed964f8a183ab79c168bfa
divykirtisrivastava <divykirtisrivastava@gmail.com>
2024-06-08 11:40:20 +0530

    add delete

--- frontend/src/components/AdminPannel.jsx
+++ frontend/src/components/AdminPannel.jsx
@@ -17,6 +17,11 @@ export default function AdminPannel() {
     setData(result.data)
   }
 
+async function deleteProduct(id){
+  console.log(id)
+  await axios.delete(`http://localhost:3000/api/deleteProduct/${id}`)
+  fetchProductData()
+}
 
 
   return (
@@ -97,7 +102,9 @@ export default function AdminPannel() {
                         </td>
                         <td className="whitespace-nowrap px-4 py-4 text-sm text-gray-700">
                           <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">View</button>
-                          <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</button>
+                          <button type="button" 
+                          onClick={()=>deleteProduct(product.id)}
+                          class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</button>
                           <button type="button" class="focus:outline-none text-black bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900">Update</button>
                         </td>
 
