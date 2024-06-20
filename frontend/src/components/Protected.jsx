db0881dabd1c50a6de8059e01a6bd294c11d4627
divykirtisrivastava <divykirtisrivastava@gmail.com>
2024-06-20 11:00:18 +0530

    add login

--- frontend/src/components/Protected.jsx
+++ frontend/src/components/Protected.jsx
@@ -4,6 +4,7 @@ import { Navigate } from 'react-router-dom'
 export default function Protected({children}) {
  let pass  = false
  if(pass){
+  
     return children
  }else{
    return <Navigate to='/admin/adminLogin'/>
