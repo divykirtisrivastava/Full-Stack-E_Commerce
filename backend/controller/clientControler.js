ef1e3e10f64b601166f911444c349f8f3796f3e7
divykirtisrivastava <divykirtisrivastava@gmail.com>
2024-07-08 11:31:24 +0530

    bcrypt login

--- backend/controller/clientControler.js
+++ backend/controller/clientControler.js
@@ -20,16 +20,22 @@ exports.clientSave = async (req, res)=>{
 
 exports.clientLogin = (req, res)=>{
     let email = req.body.email
-    let password = req.body.password
-    let sql  = "select * from clientdetail where email = ? and password  = ?"
-    db.query(sql, [email, password], (err, result)=>{
-        if(err)  throw err;
-        else{
-            if(result.length > 0){
-                res.send(true)
-            }else{
-                res.send(false)
+    let password = req.body.password  //user at login
+    let sql  = "select * from clientdetail where email = ?"
+    db.query(sql, [email], (err, result)=>{
+       
+         if(err)  throw err;else{
+            // result[0].password :- database saved password
+           bcrypt.compare(password, result[0].password, (err, isMatch)=>{
+            if(err) throw err
+            else{
+                if(isMatch == true){
+                    res.send(true)
+                }else{
+                    res.send(false)
+                }
             }
+           })
         }
     })
 
